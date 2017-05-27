0bb1b942279b860e3611a7d7cb99fd6dd4cdcecb
minutel163 <minutel@163.com>
2017-05-27 23:03:18 +0800

    minutel

--- sort/sort.cpp
+++ sort/sort.cpp
@@ -1,23 +1,35 @@
 #include <iostream>
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <time.h> 
 using namespace std;
 
 class Sort{
 public:
 	Sort(){}
     void outputArray(int arr[]){
+		cout<<"start output"<<endl;
+		int size1 = sizeof(arr);
+		int size2 = sizeof(arr[0]);
+		cout<<"size1"<<size1<<endl;
+		cout<<"size2"<<size2<<endl;
     	int len = sizeof(arr)/sizeof(arr[0]);
+		cout<<"len"<<len<<endl;
     	for(int i = 0;i<len;i++){
     		cout<< i<<" : "<<arr[i]<<endl;
     	}
     }
 };
 int main(){
-	Sort so = new Sort;
+	Sort so;
 	int len = 10;
 	int arr[len];
+	cout<<"start"<<endl;
 	for(int i = 0; i < 10 ; i++){
 		arr[i] = rand();
+		cout<<arr[i]<<endl;
 	}
+	cout<<"middle"<<endl;
 	so.outputArray(arr);
 	return 0;
 }
